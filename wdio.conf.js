5231ce2cae159bcfe667597eb409e20c5e831a5c
Eoin Corr <eoin.corr@dovehousesolutions.com>
2025-10-20 17:53:23 +0100

    test one wdio file

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,92 +1,111 @@
+// wdio.conf.js (ESM)
 import fs from 'node:fs'
+import { URL } from 'node:url'
+import allure from 'allure-commandline'
 import testConfig from './config/config.js'
 
-export const cucumberTag = process.env.ENVIRONMENT
+// ---- Environment & flags ----------------------------------------------------
+const envName = process.env.environment ?? process.env.ENVIRONMENT ?? 'dev' // default to 'dev'
+export const cucumberTag = envName
 
+const debug = !!process.env.DEBUG
+
+// ---- Base URL ---------------------------------------------------------------
+const baseUrl = `https://apha-integration-bridge.api.${envName}.cdp-int.defra.cloud`
+
+// ---- Specs selection --------------------------------------------------------
+// If a per-env folder exists (e.g. ./test/features/dev), use it. Otherwise, run all.
+const perEnvSpecsDir = `./test/features/${envName}`
+const specs = fs.existsSync(perEnvSpecsDir)
+  ? [`${perEnvSpecsDir}/*.feature`]
+  : ['./test/features/**/*.feature']
+
+// ---- Proxy (Chrome) ---------------------------------------------------------
 let chromeProxyConfig = {}
 if (process.env.HTTP_PROXY) {
   const url = new URL(process.env.HTTP_PROXY)
+  const hostPort = url.port ? `${url.hostname}:${url.port}` : url.hostname
   chromeProxyConfig = {
     proxy: {
       proxyType: 'manual',
-      httpProxy: `${url.host}:${url.port}`,
-      sslProxy: `${url.host}:${url.port}`
+      httpProxy: hostPort,
+      sslProxy: hostPort
     }
   }
 }
 
+// ---- Capabilities -----------------------------------------------------------
+const headlessArgs = [
+  '--no-sandbox',
+  '--disable-infobars',
+  '--headless',
+  '--disable-gpu',
+  '--window-size=1920,1080',
+  '--enable-features=NetworkService,NetworkServiceInProcess',
+  '--password-store=basic',
+  '--use-mock-keychain',
+  '--dns-prefetch-disable',
+  '--disable-background-networking',
+  '--disable-remote-fonts',
+  '--ignore-certificate-errors',
+  '--disable-dev-shm-usage'
+]
+
+const capabilities = debug
+  ? [
+      {
+        browserName: 'chrome'
+      }
+    ]
+  : [
+      {
+        ...chromeProxyConfig,
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: headlessArgs
+        },
+        maxInstances: 1
+      }
+    ]
+
+// ---- Config -----------------------------------------------------------------
 export const config = {
-  //
-  // ====================f
-  // Runner Configuration
-  // ====================
-  // WebdriverIO supports running e2e tests as well as unit and component tests.
   runner: 'local',
-  headless: true,
-  //
-  // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-  // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-  // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-  // gets prepended directly.
-  // baseUrl: `https://apha-integration-bridge-jny-test.${process.env.ENVIRONMENT}.cdp-int.defra.cloud`,
-  baseUrl: `https://apha-integration-bridge.api.${cucumberTag}.cdp-int.defra.cloud`,
+  headless: !debug,
 
-  // Connection to remote chromedriver
+  // Base URL & WD
+  baseUrl,
   hostname: process.env.CHROMEDRIVER_URL || '127.0.0.1',
-  port: process.env.CHROMEDRIVER_PORT || 4444,
+  port: Number(process.env.CHROMEDRIVER_PORT || 4444),
 
-  // Tests to run
-  // Tests to run
-  specs: ['./test/features/**/*.feature'],
+  // Specs & Cucumber
+  specs,
+  exclude: [],
+  maxInstances: 1,
+  framework: 'cucumber',
   cucumberOpts: {
     require: ['./test/step-definitions/*.js'],
     format: ['pretty', 'progress', 'summary'],
     tags: [`@${cucumberTag}`],
-    timeout: 60000
+    timeout: 60_000
   },
-  // Tests to exclude
-  exclude: [],
-  maxInstances: 1,
-  capabilities: [
-    {
-      ...chromeProxyConfig,
-      ...{
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-          args: [
-            '--no-sandbox',
-            '--disable-infobars',
-            '--headless',
-            '--disable-gpu',
-            '--window-size=1920,1080',
-            '--enable-features=NetworkService,NetworkServiceInProcess',
-            '--password-store=basic',
-            '--use-mock-keychain',
-            '--dns-prefetch-disable',
-            '--disable-background-networking',
-            '--disable-remote-fonts',
-            '--ignore-certificate-errors',
-            '--disable-dev-shm-usage'
-          ]
-        }
-      }
-    }
-  ],
 
+  // Caps
+  capabilities,
+
+  // Logging / timeouts
   logLevel: testConfig.logLevel,
-
   logLevels: {
     webdriver: 'error'
   },
-
-  // Number of failures before the test suite bails.
   bail: testConfig.bail,
   waitforTimeout: testConfig.waitForTimeout,
   waitforInterval: testConfig.waitforInterval,
   connectionRetryTimeout: testConfig.connectionRetryTimeout,
   connectionRetryCount: testConfig.connectionRetryCount,
-  framework: 'cucumber',
 
+  services: [['chromedriver', {}]],
+  // Reporters
   reporters: [
     'spec',
     [
@@ -100,156 +119,33 @@ export const config = {
     ]
   ],
 
-  // Options to be passed to Mocha.
-  // See the full list at http://mochajs.org/
-  // mochaOpts: {
-  //   ui: 'bdd',
-  //   timeout: oneMinute
-  // },
-  //
-  // =====
-  // Hooks
-  // =====
-  // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
-  // it and to build services around it. You can either apply a single function or an array of
-  // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
-  // resolved to continue.
-  /**
-   * Gets executed once before all workers get launched.
-   * @param {object} config wdio configuration object
-   * @param {Array.<Object>} capabilities list of capabilities details
-   */
-  // onPrepare: function (config, capabilities) {},
-  /**
-   * Gets executed before a worker process is spawned and can be used to initialise specific service
-   * for that worker as well as modify runtime environments in an async fashion.
-   * @param  {string} cid      capability id (e.g 0-0)
-   * @param  {object} caps     object containing capabilities for session that will be spawn in the worker
-   * @param  {object} specs    specs to be run in the worker process
-   * @param  {object} args     object that will be merged with the main configuration once worker is initialized
-   * @param  {object} execArgv list of string arguments passed to the worker process
-   */
-  // onWorkerStart: function (cid, caps, specs, args, execArgv) {},
-  /**
-   * Gets executed just after a worker process has exited.
-   * @param  {string} cid      capability id (e.g 0-0)
-   * @param  {number} exitCode 0 - success, 1 - fail
-   * @param  {object} specs    specs to be run in the worker process
-   * @param  {number} retries  number of retries used
-   */
-  // onWorkerEnd: function (cid, exitCode, specs, retries) {},
-  /**
-   * Gets executed just before initialising the webdriver session and test framework. It allows you
-   * to manipulate configurations depending on the capability or spec.
-   * @param {object} config wdio configuration object
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {Array.<String>} specs List of spec file paths that are to be run
-   * @param {string} cid worker id (e.g. 0-0)
-   */
-  // beforeSession: function (config, capabilities, specs, cid) {},
-  /**
-   * Gets executed before test execution begins. At this point you can access to all global
-   * variables like `browser`. It is the perfect place to define custom commands.
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {Array.<String>} specs        List of spec file paths that are to be run
-   * @param {object}         browser      instance of created browser/device session
-   */
-  // before: function (capabilities, specs) {},
-  /**
-   * Runs before a WebdriverIO command gets executed.
-   * @param {string} commandName hook command name
-   * @param {Array} args arguments that command would receive
-   */
-  // beforeCommand: function (commandName, args) {},
-  /**
-   * Hook that gets executed before the suite starts
-   * @param {object} suite suite details
-   */
-  // beforeSuite: function (suite) {},
-  /**
-   * Function to be executed before a test (in Mocha/Jasmine) starts.
-   */
-  // beforeTest: function (test, context) {},
-  /**
-   * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
-   * beforeEach in Mocha)
-   */
-  // beforeHook: function (test, context) {},
-  /**
-   * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
-   * afterEach in Mocha)
-   */
-  // afterHook: function (
-  //   test,
-  //   context,
-  //   { error, result, duration, passed, retries }
-  // ) {},
-  /**
-   * Function to be executed after a test (in Mocha/Jasmine only)
-   * @param {object}  test             test object
-   * @param {object}  context          scope object the test was executed with
-   * @param {Error}   result.error     error object in case the test fails, otherwise `undefined`
-   * @param {*}       result.result    return object of test function
-   * @param {number}  result.duration  duration of test
-   * @param {boolean} result.passed    true if test has passed, otherwise false
-   * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
-   */
-  afterTest: async function (
-    test,
-    context,
-    { error, result, duration, passed, retries }
-  ) {
+  afterTest: async function (_test, _context, { error }) {
     if (error) {
       await browser.takeScreenshot()
     }
   },
 
-  /**
-   * Hook that gets executed after the suite has ended
-   * @param {object} suite suite details
-   */
-  // afterSuite: function (suite) {},
-  /**
-   * Runs after a WebdriverIO command gets executed
-   * @param {string} commandName hook command name
-   * @param {Array} args arguments that command would receive
-   * @param {number} result 0 - command success, 1 - command error
-   * @param {object} error error object if any
-   */
-  // afterCommand: function (commandName, args, result, error) {},
-  /**
-   * Gets executed after all tests are done. You still have access to all global variables from
-   * the test.
-   * @param {number} result 0 - test pass, 1 - test fail
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {Array.<String>} specs List of spec file paths that ran
-   */
-  // after: function (result, capabilities, specs) {},
-  /**
-   * Gets executed right after terminating the webdriver session.
-   * @param {object} config wdio configuration object
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {Array.<String>} specs List of spec file paths that ran
-   */
-  // afterSession: function (config, capabilities, specs) {},
-  /**
-   * Gets executed after all workers got shut down and the process is about to exit. An error
-   * thrown in the onComplete hook will result in the test run failing.
-   * @param {object} exitCode 0 - success, 1 - fail
-   * @param {object} config wdio configuration object
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {<Object>} results object containing test results
-   */
-  onComplete: function (exitCode, config, capabilities, results) {
-    // !Do Not Remove! Required for test status to show correctly in portal.
+  onComplete: function (_exitCode, _config, _capabilities, results) {
     if (results?.failed && results.failed > 0) {
       fs.writeFileSync('FAILED', JSON.stringify(results))
     }
+
+    const reportError = new Error('Could not generate Allure report')
+    const proc = allure([
+      'generate',
+      'allure-results',
+      '--clean',
+      '--name',
+      `APHA-Test-Results-on-environment-${cucumberTag}`
+    ])
+
+    return new Promise((resolve, reject) => {
+      const timeout = setTimeout(() => reject(reportError), 60 * 1000)
+      proc.on('exit', (code) => {
+        clearTimeout(timeout)
+        if (code !== 0) return reject(reportError)
+        resolve()
+      })
+    })
   }
-  /**
-   * Gets executed when a refresh happens.
-   * @param {string} oldSessionId session ID of the old session
-   * @param {string} newSessionId session ID of the new session
-   */
-  // onReload: function (oldSessionId, newSessionId) {}
 }
