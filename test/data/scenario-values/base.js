c36dd11d83e33b2bf8d2ff03be8bdc8927cb138c
Eoin Corr <eoin.corr@dovehousesolutions.com>
2026-03-02 17:33:24 +0000

    Customer test

--- test/data/scenario-values/base.js
+++ test/data/scenario-values/base.js
@@ -3,11 +3,13 @@ import locations from './base/locations.js'
 import workorders from './base/workorders.js'
 import usersFind from './base/users-find.js'
 import caseCreate from './base/case-create.js'
+import customersFind from './base/customers-find.js'
 
 export default {
   holdings,
   locations,
   workorders,
   usersFind,
-  caseCreate
+  caseCreate,
+  customersFind
 }
